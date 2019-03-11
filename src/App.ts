f20497de452681a6bc3a49e96ed99b590c75f08e
skugge <tor.skuggevik@gmail.com>
2019-03-11 13:19:09 +0100

    initial room class #4

--- src/App.ts
+++ src/App.ts
@@ -10,23 +10,56 @@ const server = https.createServer({
 
 class App {
     public wss: any;
+    private connections: [any];
+    private rooms: [Room];
 
     constructor() {
-        this.wss = new websocket.Server({server, rejectUnauthorized: false});
-        this.mountRoutes();
+        this.wss = new websocket.Server({ server });
+        this.listener();
         server.listen(port);
     }
-    
 
-    private mountRoutes(): void {
-        this.wss.on('connection', (ws:any) => {
-            ws.on('message', (msg:String) => {
-                console.log({message_received:msg});
+    private listener(): void {
+        this.wss.on('connection', (ws: any) => {
+            //this.findConnection(ws);
+            console.log({ conn: "new conn" });
+            this.connections.push(ws);
+            ws.on('message', (msg: String) => {
+                console.log({ message_received: msg });
             });
-
             ws.send('hello from the other side');
         });
+    }
 
+    private findConnection(conn: any): void {
+        for (let i = 0; i < this.connections.length; i++) {
+            const connection = this.connections[i];
+            const next = this.connections[i + 1];
+            if (connection.readyState === websocket.OPEN && next.readyState === websocket.OPEN) {
+                //do something
+            }
+        }
+    }
+
+}
+
+class Room {
+    private player1;
+    private player2;
+
+    constructor(conn1, conn2) {
+        this.player1 = conn1;
+        this.player2 = conn2;
+        this.handleMessage();
+    }
+
+    private handleMessage(): void {
+        this.player1.on('message', (msg) => {
+            this.player2.send(msg);
+        });
+        this.player2.on('message', (msg) => {
+            this.player1.send(msg);
+        });
     }
 }
 
