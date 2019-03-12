a1b3d4ab5f5c06972360edac2693de55f45f0aee
skugge <tor.skuggevik@gmail.com>
2019-03-12 11:11:11 +0100

    join clients #4

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8080
+const port = 8080
 import * as websocket from 'ws';
 import * as fs from 'fs';
 import * as https from 'https';
@@ -7,42 +7,6 @@ const server = https.createServer({
     cert: fs.readFileSync('certificates/server.crt', 'utf-8'),
     key: fs.readFileSync('certificates/server.key', 'utf-8'),
 });
-
-class App {
-    public wss: any;
-    private connections: [any];
-    private rooms: [Room];
-
-    constructor() {
-        this.wss = new websocket.Server({ server });
-        this.listener();
-        server.listen(port);
-    }
-
-    private listener(): void {
-        this.wss.on('connection', (ws: any) => {
-            //this.findConnection(ws);
-            console.log({ conn: "new conn" });
-            this.connections.push(ws);
-            ws.on('message', (msg: String) => {
-                console.log({ message_received: msg });
-            });
-            ws.send('hello from the other side');
-        });
-    }
-
-    private findConnection(conn: any): void {
-        for (let i = 0; i < this.connections.length; i++) {
-            const connection = this.connections[i];
-            const next = this.connections[i + 1];
-            if (connection.readyState === websocket.OPEN && next.readyState === websocket.OPEN) {
-                //do something
-            }
-        }
-    }
-
-}
-
 class Room {
     private player1;
     private player2;
@@ -62,5 +26,66 @@ class Room {
         });
     }
 }
+class App {
+    public wss: any;
+    private connections: any[];
+    private rooms: any[];
+
+    constructor() {
+        this.connections = [];
+        this.rooms = [];
+        this.wss = new websocket.Server({ server });
+        this.listener();
+        server.listen(port);
+    }
+
+    private listener(): void {
+        this.wss.on('connection', (ws: any) => {
+            ws.id = generateID(); //assign each client an id;
+            console.log({ new_conn: ws.id });
+            this.findConnection(ws);
+/*             ws.on('message', (msg: String) => {
+                console.log({ message_received: msg });
+                ws.send(msg);
+            });
+ */        });
+    }
+
+    private findConnection(conn: any): void {
+        console.log("FIND CONNECTION");
+        if (this.connections.length > 0) {
+            for (let i = 0; i < this.connections.length; i++) {
+                const conn2 = this.connections[i];
+                if (conn2.readyState === websocket.OPEN) {
+                    console.log({ findconnection: "found connection" });
+                    //remove from list of available connections
+                    for (let j = 0; j < this.connections.length; j++) {
+                        if (this.connections[j].id === conn.id) this.connections.splice(j, 1);
+                        else if (this.connections[j].id === conn2.id) this.connections.splice(j, 1);
+                    }
+                    //join connections together in room
+                    const p = new Room(conn, conn2);
+                    this.rooms.push(p);
+                    return;
+                }
+            }
+
+        }else{
+            //no available connections, wait for incoming clients
+            this.connections.push(conn);
+            console.log({no_available_connecitons:this.connections.length});
+        }
+       
+    }
+}
+
+function generateID() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+}
+
+
 
 export default new App();
